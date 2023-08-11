74c688d20141fef664eba3eb3738cfefc1656621
gauravgupta <gaurav.mobilefirst@gmail.com>
2023-08-12 01:36:27 +0530

    about-page-done

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,5 @@
 const header = document.querySelector('.header');
+const headerTranparent = document.querySelector('.header--tranparent');
 
 window.addEventListener("scroll", () => {
     if(window.scrollY > 10){
@@ -36,8 +37,31 @@ $(document).ready(function() {
 })
 
 
-
 // Swiper start
+const aboutSwiper = new Swiper(".mySwiper", {
+    // spaceBetween:20,
+    // centeredSlides:true,
+    // sliderPerView:1,
+    autoplay:{
+        delay:2500,
+        disableOnInteraction:false,
+    },
+    breakpoints:{
+        576:{
+            sliderPerView:1.5,
+            spaceBetween:10,
+        },
+        768:{
+            sliderPerView:2,
+            spaceBetween:15,
+        },
+        992:{
+            sliderPerView:3,
+            spaceBetween:20,
+        },
+    }
+})
+
 const swiper1 = new Swiper(".swiper-container", {
     grabCursor:true,
     slidePerView:1,
